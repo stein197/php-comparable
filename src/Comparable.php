3433bea71308599a4212697154786f3ac6f3a471
STEIN197 <nil20122013@gmail.com>
2021-07-07 17:24:54 +0500

    Initial commit

---
+++ src/Comparable.php
@@ -0,0 +1,18 @@
+<?php
+	namespace Stein197;
+
+	/**
+	 * This interface should be implemented by classes which instances are supposed to be compared. For instance if
+	 * these is an object collection of the same class and there is a need to sort the array. Implementing this
+	 * interface can help with this.
+	 * @package Stein197
+	 */
+	interface Comparable {
+
+		/**
+		 * Compares current object with another.
+		 * @param Comparable $o Object to compare with.
+		 * @return int `-1` if current object is less than another, `1` in other case, `0` if objects are equal.
+		 */
+		public function compareTo(Comparable $o): int;
+	}
